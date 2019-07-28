897ef544db61a043e10d7152c386c98d853d60aa
vincentouma <vinceoumah@gmail.com>
2019-07-28 22:19:20 +0300

    Create first API call

--- app/request.py
+++ app/request.py
@@ -21,3 +21,23 @@ def configure_request(app):
     base_url = app.config['NEWS_API_BASE_URL']
     article_url = app.config['BASE_URL']
 
+    def get_sources(category):
+        """
+    Function that gets the json response to our url request
+    """
+    get_sources_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        # print(get_sources_response)
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
+
+
