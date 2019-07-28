f00786ce7ea9ef0847556e77706272ec1e24a7e5
vincentouma <vinceoumah@gmail.com>
2019-07-28 23:14:20 +0300

    set app to debug mode and correct errors

--- config.py
+++ config.py
@@ -9,3 +9,30 @@ class Config:
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
